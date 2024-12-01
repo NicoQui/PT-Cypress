886226c76c89f3612b83f0126eddbf9d800f374c
Kinoniko-v1 <kinoniko123@gmail.com>
2024-12-01 00:22:42 -0300

    CP 001 casi terminado | CP 002 funcional | Falta mejorar el codigo

---
+++ cypress/e2e/home_page.cy.js
@@ -0,0 +1,79 @@
+describe('visitar pagina movistar', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+    it('Validar cuotas en compra de equipo -Cuotas.3 -Equipo.A14 ', () => {
+        // seleccion equipo
+        cy.get('#search_action')
+            .type('A14{enter}')
+
+        cy.get('.products .product-item')
+            .should('contain','A14')
+            .first()
+            .click()
+
+        cy.url().should('include','a14')
+
+        cy.get('[data-special-price]').contains('cuotas sin interés') // falta validar que sean 3
+    })
+
+    /*it('Aplicar filtro de equipos  -Memoria Interna.128GB -Precio Entre 200Ky300K', () => {
+        let minPrice = 0, maxPrice = 300000;
+        let cantFiltros = 0;
+
+        cy.get('.block-subtitle')
+            .click()
+
+        cy.get('.memory')
+            .click()
+        cy.get('[data-value="802"]')
+            .should('be.visible')
+            .click()
+
+        cy.get('.block-subtitle')
+            .click()
+        cy.get('.selectedfilters')
+            .first()
+            .should('contain','128')
+        cantFiltros++;
+
+        cy.get('.price').click() 
+        cy.get(`[data-value="${minPrice}_${maxPrice}"]`) // Aplico el filtro de la pagina (consigna = 200000_300000)
+            .should('be.visible')
+            .click()
+        cantFiltros++;
+
+        //cy.wait(5000) // espero que se apliquen los filtros
+        cy.get('.block-subtitle > span').should('contain',`${cantFiltros}`) // otra alternativa
+        
+        cy.get('.products > ol')
+            .find('.product-item')
+            .filter(':visible')
+            .should('contain','128')
+            .and('have.length.greaterThan',0)
+            .then(($li) => {
+                const cantProd = $li.length;
+                cy.get('.total-products').should('contain',`${cantProd}`);
+            })
+
+        cy.get('.special-price')
+            .invoke('text')
+            .then((priceText) => {
+                const price = parseFloat(priceText.replace('$', '').replace('.', '').replace(',', '.').trim());
+                expect(price).to.be.greaterThan(minPrice);
+                expect(price).to.be.lessThan(maxPrice);
+            })
+    })*/
+
+    /*it('Validar cuotas en compra de equipo -Cuotas.60 -Equipo.Tercero de la lista' +
+        '-Banco.Credicoop -Tarjeta.Visa', () => {
+            cy.get('.products > ol').children().eq(2).click()
+            cy.get('#open-installments-modal').click()
+            cy.get('#inputbank').type('Credicoop')
+            cy.get('#calculate_btn > .btn-primary').click()
+
+            cy.get('#ui-id-2')
+            .children()
+            .contains('Credicoop');
+        })*/
+})
---
+++ cypress/support/e2e.js
@@ -0,0 +1,28 @@
+// ***********************************************************
+// This example support/e2e.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Capturo un error que sucede varias veces y falla los test
+    // reading substring - slice
+    if (err.message.includes("Cannot read properties of undefined")) {
+      return false
+    }
+  })
